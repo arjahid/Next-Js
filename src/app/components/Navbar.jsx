5eb9013eb32b453fa485017e6b71be3036cb030e
arjahid <jhjahidhasanar@gmail.com>
2025-06-04 20:10:13 +0600

    add routing

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,13 +1,14 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Navbar() {
   return (
      <nav className='bg-gray-800 text-white p-4 flex justify-center'>
           <ul className='flex space-x-4 justify-between w-1/2'>
-            <li>Home</li>
-            <li>Contact</li>
-            <li>about</li>
-            <li>blog</li>
+            <Link href='/'><li>Home</li></Link>
+            <Link href='/contact'><li>Contact</li></Link>
+           <Link href='/about'><li>About</li></Link>
+            <Link href='/blog'><li>Blog</li></Link>
           </ul>
         </nav>
   )
