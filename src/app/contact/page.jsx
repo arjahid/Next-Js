30d385cb8e7b96a2ab709629c7774298ef894477
arjahid <jhjahidhasanar@gmail.com>
2025-06-04 21:20:00 +0600

    navigate

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,27 @@
+'use client'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
+
 import React from 'react'
 
 export default function page() {
+    const router=useRouter();
+    const isloggin=true;
+    const handleClick=()=>{
+        if(isloggin){
+            router.push('/contact/more');
+        }
+        else{
+            router.push('/login');
+        }
+
+    }
   return (
+
     <div>contact page
         <ul>
-            <Link href='/'>Home</Link>
+            <Link href='/contact/more'>more</Link>
+           <button type='button' onClick={handleClick}> </button>
         </ul>
     </div>
   )
